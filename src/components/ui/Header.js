a59c1ba45eeb6a68be46dfd73ced81051c7d1937
sossw1 <sossw1@gmail.com>
2022-04-06 14:00:59 -0400

    basic appbar

---
+++ src/components/ui/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { AppBar, Toolbar } from '@mui/material';
+
+export default function Header(props) {
+  return (
+    <AppBar position='fixed'>
+      <Toolbar>
+        Arc Development
+      </Toolbar>
+    </AppBar>
+  )
+}
