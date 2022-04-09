0d32eaf09d95c2110a710c4a9e5311683342f5b6
sossw1 <sossw1@gmail.com>
2022-04-09 11:14:15 -0400

    extract sx objects

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -15,6 +15,16 @@ function ElevationScroll(props) {
   });
 }
 
+const sxTabs = {
+  ml: 'auto'
+}
+
+const sxTab = {
+  fontFamily: 'Raleway',
+  fontWeight: 700,
+  textTransform: 'none'
+}
+
 export default function Header(props) {
   return (
     <>
@@ -23,12 +33,12 @@ export default function Header(props) {
         <AppBar>
           <Toolbar disableGutters>
             <img alt='company logo' src={logo} style={{ height: '7em' }} />
-            <Tabs sx={{ ml: 'auto' }}>
-              <Tab sx={{ fontFamily: 'Raleway', fontWeight: 700, textTransform: 'none' }} label='Home' />
-              <Tab sx={{ fontFamily: 'Raleway', fontWeight: 700, textTransform: 'none' }} label='Services' />
-              <Tab sx={{ fontFamily: 'Raleway', fontWeight: 700, textTransform: 'none' }} label='The Revolution' />
-              <Tab sx={{ fontFamily: 'Raleway', fontWeight: 700, textTransform: 'none' }} label='About Us' />
-              <Tab sx={{ fontFamily: 'Raleway', fontWeight: 700, textTransform: 'none' }} label='Contact Us' />
+            <Tabs sx={sxTabs}>
+              <Tab sx={sxTab} label='Home' />
+              <Tab sx={sxTab} label='Services' />
+              <Tab sx={sxTab} label='The Revolution' />
+              <Tab sx={sxTab} label='About Us' />
+              <Tab sx={sxTab} label='Contact Us' />
             </Tabs>
           </Toolbar>
         </AppBar>
