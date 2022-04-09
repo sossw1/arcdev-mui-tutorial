12e3aaca89bf04b74e38d91c5174e8c7160639c9
sossw1 <sossw1@gmail.com>
2022-04-09 11:04:53 -0400

    tabs in toolbar

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { AppBar, CssBaseline, Toolbar, useScrollTrigger } from '@mui/material';
+import { AppBar, CssBaseline, Tab, Tabs, Toolbar, useScrollTrigger } from '@mui/material';
 
 import logo from '../../assets/logo.svg';
 
@@ -23,6 +23,13 @@ export default function Header(props) {
         <AppBar>
           <Toolbar disableGutters>
             <img alt='company logo' src={logo} style={{ height: '7em' }} />
+            <Tabs>
+              <Tab label='Home' />
+              <Tab label='Services' />
+              <Tab label='The Revolution' />
+              <Tab label='About Us' />
+              <Tab label='Contact Us' />
+            </Tabs>
           </Toolbar>
         </AppBar>
       </ElevationScroll>
