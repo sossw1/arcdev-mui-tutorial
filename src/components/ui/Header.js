6582c603fd8709f46737adc104bd9ad3bb0bdf61
sossw1 <sossw1@gmail.com>
2022-04-19 14:44:14 -0400

    reorganize, move single use extracted sx objects back

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -17,43 +17,6 @@ function ElevationScroll(props) {
   });
 }
 
-const sxLogoButton = {
-  padding: 0
-}
-
-const sxTabs = {
-  ml: 'auto'
-}
-
-const sxButton = {
-  ...theme.typography.estimate,
-  borderRadius: '50px',
-  ml: '25px',
-  mr: '25px',
-  height: '45px',
-  lineHeight: '1rem'
-}
-
-const sxPaper = {
-  sx: {
-    backgroundColor: 'common.blue',
-    color: 'common.white',
-    borderRadius: 0
-  }
-}
-
-const sxMenuItem = {
-  ...theme.typography.tab,
-  opacity: 0.7,
-  '&:hover': {
-    opacity: 1
-  },
-  '&.Mui-selected': {
-    color: '#FFBA60',
-    opacity: 1
-  }
-}
-
 const StyledSvg = styled('svg')(({ theme }) => ({
   height: '8em',
   textTransform: 'none',
@@ -135,18 +98,6 @@ export default function Header(props) {
     }
   }, [location.pathname, value]);
 
-  const sxTab = (val, opaqueOnHover = false) => {
-    return {
-      ...theme.typography.tab,
-      minWidth: 10,
-      ml: '1.5rem',
-      opacity: (value === val ? 1 : 0.7),
-      '&:hover': {
-        opacity: (opaqueOnHover ? 0.7 : 1)
-      }
-    }
-  }
-
   const handleChange = (event, value) => {
     setValue(value);
   }
@@ -186,13 +137,25 @@ export default function Header(props) {
     }
   ];
 
+  const sxTab = (val, opaqueOnHover = false) => {
+    return {
+      ...theme.typography.tab,
+      minWidth: 10,
+      ml: '1.5rem',
+      opacity: (value === val ? 1 : 0.7),
+      '&:hover': {
+        opacity: (opaqueOnHover ? 0.7 : 1)
+      }
+    }
+  }
+
   const tabs = (
     <>
       <Tabs
         value={value}
         textColor={'secondary'}
         onChange={handleChange}
-        sx={sxTabs}
+        sx={{ ml: 'auto' }}
         aria-label="navigation tabs"
       >
         <Tab
@@ -234,7 +197,18 @@ export default function Header(props) {
           disableRipple
         />
       </Tabs>
-      <Button sx={sxButton} variant='contained' color='secondary'>
+      <Button
+        sx={{
+          ...theme.typography.estimate,
+          borderRadius: '50px',
+          ml: '25px',
+          mr: '25px',
+          height: '45px',
+          lineHeight: '1rem'
+        }}
+        variant='contained'
+        color='secondary'
+      >
         Free Estimate
       </Button>
       <Menu
@@ -243,7 +217,13 @@ export default function Header(props) {
         open={open}
         onClose={handleClose}
         MenuListProps={{ onMouseLeave: handleClose }}
-        PaperProps={sxPaper}
+        PaperProps={{
+          sx: {
+            backgroundColor: 'common.blue',
+            color: 'common.white',
+            borderRadius: 0
+          }
+        }}
         elevation={0}
       >
         {menuOptions.map((option, index) =>
@@ -256,7 +236,17 @@ export default function Header(props) {
             selected={index === selectedIndex && value === 1}
             component={Link}
             to={option.link}
-            sx={sxMenuItem}
+            sx={{
+              ...theme.typography.tab,
+              opacity: 0.7,
+              '&:hover': {
+                opacity: 1
+              },
+              '&.Mui-selected': {
+                color: '#FFBA60',
+                opacity: 1
+              }
+            }}
           >
             {option.name}
           </MenuItem>
@@ -274,7 +264,7 @@ export default function Header(props) {
             <Button
               component={Link}
               to='/'
-              sx={sxLogoButton}
+              sx={{ padding: 0 }}
               disableRipple
             >
               <StyledSvg id="Layer_1" alt="company logo" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 480 139"><style>{`.st0{fill:none}.st1{fill:#fff}.st2{font-family:Raleway; font-weight: 100;}.st6{fill:none;stroke:#000;stroke-width:3;stroke-miterlimit:10}`}</style><path d="M448.07-1l-9.62 17.24-8.36 14.96L369.93 139H-1V-1z" /><path className="st0" d="M-1 139h479.92v.01H-1z" /><text transform="translate(261.994 65.233)" className="st1 st2" fontSize="57">Arc</text><text transform="translate(17.692 112.015)" className="st1 st2" fontSize="54">Development</text><path className="st0" d="M382.44 116.43l47.65-85.23 8.36-14.96M369.83 139l-.01.01L362 153" /><path d="M438.76 15.76l-56.42 100.91c-12.52-10.83-20.45-26.82-20.45-44.67 0-32.58 26.42-59 59-59 6.23 0 12.24.97 17.87 2.76z" fill="#0b72b9" /><path d="M479.89 72c0 32.58-26.42 59-59 59-14.73 0-28.21-5.4-38.55-14.33l56.42-100.91c23.85 7.57 41.13 29.89 41.13 56.24z" /><g id="Group_186" transform="translate(30.153 11.413)"><g id="Group_185"><g id="Words"><path id="Path_59" className="st1" d="M405.05 14.4l-.09 80.38-7.67-.01.06-52.25-29.4 52.21-7.94-.01 45.04-80.32z" /></g></g></g><path className="st0" d="M457-17l-8.93 16-9.62 17.24-8.36 14.96L369.93 139l-.01.01L361 155" /></StyledSvg>
