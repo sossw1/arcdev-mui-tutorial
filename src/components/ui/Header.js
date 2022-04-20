970171d1446947f6e124f96f7cce82674207df35
sossw1 <sossw1@gmail.com>
2022-04-19 22:31:12 -0400

    remove drawer top padding

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -300,7 +300,7 @@ export default function Header(props) {
           }
         }}
       >
-        <List>
+        <List disablePadding>
           {listItems.map((item, index) => (
             <ListItem
               key={index}
