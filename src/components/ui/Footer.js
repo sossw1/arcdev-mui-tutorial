56219a05de74331d0b852b6353b35349b9e40e14
sossw1 <sossw1@gmail.com>
2022-04-20 20:01:53 -0400

    hide footer links below large breakpoint

--- src/components/ui/Footer.js
+++ src/components/ui/Footer.js
@@ -1,4 +1,4 @@
-import { Grid } from '@mui/material';
+import { Grid, Hidden } from '@mui/material';
 import { styled } from '@mui/material/styles';
 import { Link } from 'react-router-dom';
 
@@ -32,136 +32,138 @@ export default function Footer(props) {
   const { setValue, setSelectedMenuIndex } = props;
   return (
     <StyledFooter>
-      <Grid
-        container
-        sx={{
-          position: 'absolute',
-          justifyContent: 'center'
-        }}
-      >
-        <Grid item sx={{ m: '3em' }}>
-          <Grid container spacing={2} direction='column'>
-            <GridLink
-              item
-              component={Link}
-              onClick={() => setValue(0)}
-              to='/'
-            >Home</GridLink>
+      <Hidden lgDown>
+        <Grid
+          container
+          sx={{
+            position: 'absolute',
+            justifyContent: 'center'
+          }}
+        >
+          <Grid item sx={{ m: '3em' }}>
+            <Grid container spacing={2} direction='column'>
+              <GridLink
+                item
+                component={Link}
+                onClick={() => setValue(0)}
+                to='/'
+              >Home</GridLink>
+            </Grid>
+          </Grid>
+          <Grid item sx={{ m: '3em' }}>
+            <Grid container spacing={2} direction='column'>
+              <GridLink
+                item
+                component={Link}
+                onClick={() => { setValue(1); setSelectedMenuIndex(0) }}
+                to='/services'
+              >
+                Services
+              </GridLink>
+              <GridLink
+                item
+                component={Link}
+                onClick={() => { setValue(1); setSelectedMenuIndex(1); }}
+                to='/custom-software'
+              >
+                Custom Software Development
+              </GridLink>
+              <GridLink
+                item
+                component={Link}
+                onClick={() => { setValue(1); setSelectedMenuIndex(2) }}
+                to='/mobile-apps'
+              >
+                Mobile App Development
+              </GridLink>
+              <GridLink
+                item
+                component={Link}
+                onClick={() => { setValue(1); setSelectedMenuIndex(3) }}
+                to='/websites'
+              >
+                Website Development
+              </GridLink>
+            </Grid>
+          </Grid>
+          <Grid item sx={{ m: '3em' }}>
+            <Grid container spacing={2} direction='column'>
+              <GridLink
+                item
+                component={Link}
+                onClick={() => setValue(2)}
+                to='/revolution'
+              >
+                The Revolution
+              </GridLink>
+              <GridLink
+                item
+                component={Link}
+                onClick={() => setValue(2)}
+                to='/revolution'
+              >
+                Vision
+              </GridLink>
+              <GridLink
+                item
+                component={Link}
+                onClick={() => setValue(2)}
+                to='/revolution'
+              >
+                Technology
+              </GridLink>
+              <GridLink
+                item
+                component={Link}
+                onClick={() => setValue(2)}
+                to='/revolution'
+              >
+                Process
+              </GridLink>
+            </Grid>
+          </Grid>
+          <Grid item sx={{ m: '3em' }}>
+            <Grid container spacing={2} direction='column'>
+              <GridLink
+                item
+                component={Link}
+                onClick={() => setValue(3)}
+                to='/about'
+              >
+                About Us
+              </GridLink>
+              <GridLink
+                item
+                component={Link}
+                onClick={() => setValue(3)}
+                to='/about'
+              >
+                History
+              </GridLink>
+              <GridLink
+                item
+                component={Link}
+                onClick={() => setValue(3)}
+                to='/about'
+              >
+                Team
+              </GridLink>
+            </Grid>
+          </Grid>
+          <Grid item sx={{ m: '3em' }}>
+            <Grid container spacing={2} direction='column'>
+              <GridLink
+                item
+                component={Link}
+                onClick={() => setValue(4)}
+                to='/contact'
+              >
+                Contact
+              </GridLink>
+            </Grid>
           </Grid>
         </Grid>
-        <Grid item sx={{ m: '3em' }}>
-          <Grid container spacing={2} direction='column'>
-            <GridLink
-              item
-              component={Link}
-              onClick={() => { setValue(1); setSelectedMenuIndex(0) }}
-              to='/services'
-            >
-              Services
-            </GridLink>
-            <GridLink
-              item
-              component={Link}
-              onClick={() => { setValue(1); setSelectedMenuIndex(1); }}
-              to='/custom-software'
-            >
-              Custom Software Development
-            </GridLink>
-            <GridLink
-              item
-              component={Link}
-              onClick={() => { setValue(1); setSelectedMenuIndex(2) }}
-              to='/mobile-apps'
-            >
-              Mobile App Development
-            </GridLink>
-            <GridLink
-              item
-              component={Link}
-              onClick={() => { setValue(1); setSelectedMenuIndex(3) }}
-              to='/websites'
-            >
-              Website Development
-            </GridLink>
-          </Grid>
-        </Grid>
-        <Grid item sx={{ m: '3em' }}>
-          <Grid container spacing={2} direction='column'>
-            <GridLink
-              item
-              component={Link}
-              onClick={() => setValue(2)}
-              to='/revolution'
-            >
-              The Revolution
-            </GridLink>
-            <GridLink
-              item
-              component={Link}
-              onClick={() => setValue(2)}
-              to='/revolution'
-            >
-              Vision
-            </GridLink>
-            <GridLink
-              item
-              component={Link}
-              onClick={() => setValue(2)}
-              to='/revolution'
-            >
-              Technology
-            </GridLink>
-            <GridLink
-              item
-              component={Link}
-              onClick={() => setValue(2)}
-              to='/revolution'
-            >
-              Process
-            </GridLink>
-          </Grid>
-        </Grid>
-        <Grid item sx={{ m: '3em' }}>
-          <Grid container spacing={2} direction='column'>
-            <GridLink
-              item
-              component={Link}
-              onClick={() => setValue(3)}
-              to='/about'
-            >
-              About Us
-            </GridLink>
-            <GridLink
-              item
-              component={Link}
-              onClick={() => setValue(3)}
-              to='/about'
-            >
-              History
-            </GridLink>
-            <GridLink
-              item
-              component={Link}
-              onClick={() => setValue(3)}
-              to='/about'
-            >
-              Team
-            </GridLink>
-          </Grid>
-        </Grid>
-        <Grid item sx={{ m: '3em' }}>
-          <Grid container spacing={2} direction='column'>
-            <GridLink
-              item
-              component={Link}
-              onClick={() => setValue(4)}
-              to='/contact'
-            >
-              Contact
-            </GridLink>
-          </Grid>
-        </Grid>
-      </Grid>
+      </Hidden>
       <StyledSvg id="Layer_1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 294.4 180"><style>{`.st1{fill:#fff}.st2{font-family:&apos;ArialMT&apos;}.st3{font-size:8px}`}</style><path d="M294.4 0L166.3 227H-4V0z" /><path fill="none" d="M111-24v376" /><text transform="translate(5 175)" className="st1 st2 st3">© Arc Development 2019</text><text transform="translate(147.442 175)" className="st1 st2 st3">Wichita, KS</text></StyledSvg>
     </StyledFooter>
   )
