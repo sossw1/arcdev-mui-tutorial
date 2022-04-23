7cb2431f2c9b526f08e54e57dadf8b2b56512346
sossw1 <sossw1@gmail.com>
2022-04-23 09:42:53 -0400

    title and buttons

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,4 +1,5 @@
 import Lottie from 'react-lottie';
+import { Button, Grid, Typography } from '@mui/material';
 
 import animationData from '../animations/landinganimation/data';
 
@@ -13,6 +14,25 @@ export default function LandingPage() {
   }
 
   return (
-    <Lottie options={defaultOptions} height={'100%'} width={'100%'} />
+    <Grid container direction='column'>
+      <Grid item>
+        <Grid container direction='row'>
+          <Grid item>
+            <Typography>
+              Bringing West Coast Technology<br />to the Midwest
+            </Typography>
+            <Grid container>
+              <Grid item>
+                <Button variant='contained'>Free Estimate</Button>
+              </Grid>
+              <Grid item>
+                <Button variant='outlined'>Learn More</Button>
+              </Grid>
+            </Grid>
+            <Lottie options={defaultOptions} height={'100%'} width={'100%'} />
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
   )
 };
